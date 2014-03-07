ea81ecd27738281dd7f0464373cc9425b4e29e02
Scott Hardy <scott.the.hardy@gmail.com>
2014-03-06 20:34:57 -0800

    made navigation controller bar red

---
+++ MobSync/MobSyncNavigationController.m
@@ -0,0 +1,25 @@
+//
+//  MobSyncNavigationController.m
+//  MobSync
+//
+//  Created by Scott Hardy on 3/6/14.
+//  Copyright (c) 2014 PWNZONE. All rights reserved.
+//
+
+#import "MobSyncNavigationController.h"
+
+@interface MobSyncNavigationController ()
+
+@end
+
+@implementation MobSyncNavigationController
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.navigationBar.barTintColor = [UIColor redColor];
+    self.navigationBar.barStyle = UIBarStyleBlack;
+}
+
+@end
