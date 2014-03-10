028827ad4e833278b142c6b0509de6decc066d8d
Matthew Medal <mmedal@ebay.com>
2014-03-09 19:06:19 -0700

    -started friend view work

---
+++ MobSync/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  MobSync
+//
+//  Created by Medal, Matthew on 3/9/14.
+//  Copyright (c) 2014 PWNZONE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UIViewController
+
+@end
---
+++ MobSync/FriendsViewController.m
@@ -0,0 +1,38 @@
+//
+//  FriendsViewController.m
+//  MobSync
+//
+//  Created by Medal, Matthew on 3/9/14.
+//  Copyright (c) 2014 PWNZONE. All rights reserved.
+//
+
+#import "FriendsViewController.h"
+
+@interface FriendsViewController ()
+
+@end
+
+@implementation FriendsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
